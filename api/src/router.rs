ddf4dfafe0b53a8f84ac316cf833e8623526dafc
intfish123 <intfish@qq.com>
2025-04-17 15:44:19 +0800

    feat: 响应体header跨域设置

--- api/src/router.rs
+++ api/src/router.rs
@@ -1,21 +1,38 @@
-use std::future::ready;
-use volo_http::{Extension, Router};
-use volo_http::server::middleware;
-use volo_http::server::route::get;
-use crate::{controller, ServiceContext};
 use crate::prometheus::{setup_metrics_recorder, track_metrics};
+use crate::{controller, ServiceContext};
+use std::future::ready;
+use volo_http::{
+    response::ServerResponse,
+    server::{middleware, route::get, IntoResponse},
+    Extension, Router,
+};
 
 /// 构建路由
 pub fn build_router(cxt: ServiceContext) -> Router {
-
     let record_handler = setup_metrics_recorder();
 
     Router::new()
         .route("/metrics", get(move || ready(record_handler.render())))
-        .route("/user/query-one", get(controller::user_controller::get_user))
-        .route("/order/query-one", get(controller::order_controller::get_order))
+        .route(
+            "/user/query-one",
+            get(controller::user_controller::get_user),
+        )
+        .route(
+            "/order/query-one",
+            get(controller::order_controller::get_order),
+        )
         .layer(middleware::from_fn(track_metrics))
+        .layer(middleware::map_response(headers_map_response))
         .layer(Extension(cxt))
 }
 
-
+async fn headers_map_response(response: ServerResponse) -> impl IntoResponse {
+    (
+        [
+            ("Access-Control-Allow-Origin", "*"),
+            ("Access-Control-Allow-Headers", "*"),
+            ("Access-Control-Allow-Method", "*"),
+        ],
+        response,
+    )
+}
