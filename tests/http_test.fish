effb107c1798671cd4bf6ffd51d22fc55eccace3
seven <seve_tis@proton.me>
2025-04-22 03:32:55 +0800

    update: test

--- tests/http_test.fish
+++ tests/http_test.fish
@@ -2,16 +2,33 @@
 
 set BASE_URL "http://localhost:8080"
 
+if test (count $argv) -eq 0
+    set DURATION 60
+else
+    set DURATION $argv[1]
+end
+
 function test_get_user
     echo "Testing get_user endpoint..."
     curl -X GET "$BASE_URL/user/query-one?id=1" -H "Content-Type: application/json"
+    echo -e "\n"
 end
 
 function test_get_order
     echo "Testing get_order endpoint..."
     curl -X GET "$BASE_URL/order/query-one?id=1" -H "Content-Type: application/json"
+    echo -e "\n"
 end
 
-test_get_user
-test_get_order
+set END_TIME (math (date +%s) + $DURATION)
 
+echo "Starting API tests for $DURATION seconds..."
+
+while test (date +%s) -lt $END_TIME
+    echo "Starting new test cycle at" (date)
+    test_get_user
+    test_get_order
+    echo "----------------------------------------"
+end
+
+echo "Finished testing after $DURATION seconds"
