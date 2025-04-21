9cb69afe459680f1c6cdcdabe00961a774b0b9e9
seven <seve_tis@proton.me>
2025-04-22 04:37:30 +0800

    feat: add metric for observe

--- api/src/prometheus.rs
+++ api/src/prometheus.rs
@@ -1,10 +1,4 @@
 use metrics_exporter_prometheus::{Matcher, PrometheusBuilder, PrometheusHandle};
-use std::time::Instant;
-use volo_http::http::{StatusCode, Uri};
-use volo_http::request::ServerRequest;
-use volo_http::response::ServerResponse;
-use volo_http::server::IntoResponse;
-use volo_http::{context::ServerContext, server::middleware::Next};
 
 pub fn setup_metrics_recorder() -> PrometheusHandle {
     const EXPONENTIAL_SECONDS: &[f64] = &[
@@ -19,34 +13,4 @@ pub fn setup_metrics_recorder() -> PrometheusHandle {
         .unwrap()
         .install_recorder()
         .unwrap()
-}
-pub async fn track_metrics(
-    uri: Uri,
-    cx: &mut ServerContext,
-    req: ServerRequest,
-    next: Next,
-) -> Result<ServerResponse, StatusCode> {
-    let start = Instant::now();
-
-    let path = uri.path();
-    let method = req.method().clone();
-
-    let response = next.run(cx, req).await;
-
-    let latency = start.elapsed().as_secs_f64();
-
-    let Ok(r) = response else {
-        return Ok(response.into_response());
-    };
-
-    let labels = [
-        ("method", method.to_string()),
-        ("path", path.to_string()),
-        ("status", r.status().as_u16().to_string()),
-    ];
-
-    metrics::counter!("http_requests_total", &labels).increment(1);
-    metrics::histogram!("http_requests_duration_seconds", &labels).record(latency);
-
-    Ok(r)
 }
